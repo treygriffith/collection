22b1a7421c3a4b0121ae023f921f56ffcf9428ee
Trey Griffith <trey.griffith@gmail.com>
2013-09-10 23:37:06 -0700

    remove autosave tests

--- test/tests.js
+++ test/tests.js
@@ -153,52 +153,6 @@ describe('model-collection', function () {
     assert(user.attrs.todos[1] === 1);
   });
 
-  it('saves new and updated models when the parent model is saved', function () {
-
-    var Todo = model('todo')
-      .attr('id')
-      .attr('name')
-      .attr('content');
-
-    var User = model('user')
-      .use(collection)
-      .collection('todos', Todo);
-
-    var user = new User();
-    var todo1 = new Todo({
-      id: 1
-      name: "something",
-      content: "something else"
-    });
-    var todo2 = new Todo({
-      id: 2
-      name: "something",
-      content: "something else"
-    });
-
-    user.todos().add(todo1);
-    user.todos().add(todo2);
-    todo2.name("blah");
-    user.todos().add({
-      name: "blahblah",
-      content: "blahblahblah"
-    });
-
-    todo1.on('saving', function () {
-      assert(false);
-    });
-
-    todo2.on('saving', function () {
-      assert(true);
-    });
-
-    user.todos().last().on('saving', function () {
-      assert(true);
-    });
-
-    user.emit('saving');
-  });
-
   it('removes destroyed models from the collection', function () {
 
     var Todo = model('todo')
@@ -227,35 +181,6 @@ describe('model-collection', function () {
 
     assert(user.todos().length() === 1);
     assert(user.todos().first().id() === 2);
-  });
-
-  it('replaces placeholders on the model\'s first save', function () {
-
-    var Todo = model('todo')
-      .attr('id')
-      .attr('name')
-      .attr('content');
-
-    var User = model('user')
-      .use(collection)
-      .collection('todos', Todo);
-
-    var user = new User();
-
-    user.todos([{
-      title: "Something",
-      content: "blah"
-    }]);
-
-    var placeholder = user.attrs.todos[0];
-
-    user.todos().first().on('save', function () {
-      assert(user.todos().first().id() === 1);
-    });
-
-    user.todos().first().id(1); // add an id (which the db would)
-    user.todos().first().emit('save'); // simulate save
-
   });
 
 
